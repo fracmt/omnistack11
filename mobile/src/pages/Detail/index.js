9a0c5693fc90fd851cd232d5eb315d22d96ad8ab
Francisco Torres <fracmt@gmail.com>
2020-04-04 22:11:35 -0300

    Mobile version implemented

--- mobile/src/pages/Detail/index.js
+++ mobile/src/pages/Detail/index.js
@@ -1,8 +1,80 @@
-import React from 'react';
-import { View } from 'react-native';
+import React from "react";
+import { Feather } from "@expo/vector-icons";
+import { useNavigation, useRoute } from "@react-navigation/native";
+import { View, Text, Image, TouchableOpacity, Linking } from "react-native";
+import * as MailComposer from "expo-mail-composer";
+
+import logoImg from "../../assets/logo.png";
+
+import styles from "./styles";
 
 export default function Detail() {
+  const navigation = useNavigation();
+  const route = useRoute();
+
+  const incident = route.params.incident;
+
+  const valueIncidentFormatted = Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: "USD"
+  }).format(incident.value);
+
+  const message = `Hello ${incident.name}, I am contacting you because I like to help in case "${incident.title}" with the value of ${valueIncidentFormatted}.`;
+
+  function navigateBack() {
+    navigation.goBack();
+  }
+
+  function sendMail() {
+    MailComposer.composeAsync({
+      subject: `Case's Hero: ${incident.title}`,
+      recipients: [incident.email],
+      body: message
+    });
+  }
+
+  function sendWhatsApp() {
+    Linking.openURL(`whatsapp://send?phone=${incident.whatsapp}&text=${message}`);
+  }
+
   return (
-    <View />
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <Image source={logoImg} />
+        <TouchableOpacity style={styles.detailsButton} onPress={navigateBack}>
+          <Feather name="arrow-left" size={28} color="#E82041" />
+        </TouchableOpacity>
+      </View>
+
+      <View style={styles.incident}>
+        <Text style={[styles.incidentProperty, { marginTop: 0 }]}>NGO:</Text>
+        <Text style={styles.incidentValue}>
+          {incident.name} of {incident.city}/{incident.state}
+        </Text>
+
+        <Text style={styles.incidentProperty}>Case:</Text>
+        <Text style={styles.incidentValue}>{incident.title}</Text>
+
+        <Text style={styles.incidentProperty}>Value:</Text>
+        <Text style={styles.incidentValue}>{valueIncidentFormatted}</Text>
+      </View>
+
+      <View style={styles.contactBox}>
+        <Text style={styles.heroTitle}>Save the day!</Text>
+        <Text style={styles.heroTitle}>Be the hero of this case.</Text>
+
+        <Text style={styles.heroDescription}>Contact us:</Text>
+
+        <View style={styles.actions}>
+          <TouchableOpacity style={styles.action} onPress={sendWhatsApp}>
+            <Text style={styles.actionText}>WhatsApp</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity style={styles.action} onPress={sendMail}>
+            <Text style={styles.actionText}>Email</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    </View>
   );
 }
--- mobile/src/pages/Incident/index.js
+++ mobile/src/pages/Incident/index.js
@@ -1,8 +1,99 @@
-import React from 'react';
-import { View } from 'react-native';
+import React, { useState, useEffect } from "react";
+import { Feather } from "@expo/vector-icons";
+import { useNavigation } from "@react-navigation/native";
+import { View, FlatList, Image, Text, TouchableOpacity } from "react-native";
+
+import api from "../../services/api";
+
+import logoImg from "../../assets/logo.png";
+
+import styles from "./styles";
 
 export default function Incident() {
+  const [incidents, setIncidents] = useState([]);
+  const [totalIncidents, setTotalIncidents] = useState(0);
+  
+  const [page, setPage] = useState(1);
+  const [loading, setLoading] = useState(false);
+
+  const navigation = useNavigation();
+
+  function navigateToDetail(incident) {
+    navigation.navigate("Detail", { incident });
+  }
+
+  async function loadIncidents() {
+    if(loading) {
+      return;
+    }
+
+    if(totalIncidents > 0 && incidents.length == totalIncidents) {
+      return;
+    }
+
+    setLoading(true);
+
+    const response = await api.get("incidents", {
+      params: { page }
+    });
+
+    setIncidents([...incidents, ...response.data]);
+    setTotalIncidents(response.headers['x-total-count']);
+    setPage(page + 1);
+    setLoading(false);
+  }
+
+  useEffect(() => {
+    loadIncidents();
+  }, []);
+
   return (
-    <View />
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <Image source={logoImg} />
+        <Text style={styles.headerText}>
+          Total of <Text style={styles.headerTextBold}>{totalIncidents} cases</Text>.
+        </Text>
+      </View>
+
+      <Text style={styles.title}>Welcome!</Text>
+      <Text style={styles.description}>
+        Choose one of the cases below and save the day!
+      </Text>
+
+      <FlatList
+        data={incidents}
+        keyExtractor={incident => String(incident.id)}
+        showsVerticalScrollIndicator={false}
+        onEndReached={loadIncidents}
+        onEndReachedThreshold={0.2}
+        style={styles.incidentList}
+        renderItem={({ item: incident }) => (
+          <View style={styles.incident}>
+            <Text style={styles.incidentProperty}>NGO:</Text>
+            <Text style={styles.incidentValue}>{incident.name}</Text>
+
+            <Text style={styles.incidentProperty}>Case:</Text>
+            <Text style={styles.incidentValue}>{incident.title}</Text>
+
+            <Text style={styles.incidentProperty}>Value:</Text>
+            <Text style={styles.incidentValue}>
+              {Intl.NumberFormat("en-US", {
+                style: "currency",
+                currency: "USD"
+              }).format(incident.value)}
+            </Text>
+
+            <TouchableOpacity
+              style={styles.detailsButton}
+              onPress={() => navigateToDetail(incident)}
+            >
+              <Text style={styles.detailsButtonText}>See more details</Text>
+              <Feather name="arrow-right" size={16} color="#E82041" />
+            </TouchableOpacity>
+          </View>
+        )}
+      />
+    </View>
   );
 }
